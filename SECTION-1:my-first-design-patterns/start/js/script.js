d78b66278e75c060b7d459f897fd3fa319c3868f
Alberto Cueto <cueto.alberto@gmail.com>
2016-11-01 23:33:34 -0600

    Adding the module pattern to ensure private variables and methods, and return as accessible objects just the elements we want everynone to see

--- SECTION-1:my-first-design-patterns/start/js/script.js
+++ SECTION-1:my-first-design-patterns/start/js/script.js
@@ -1,17 +1,66 @@
 //Al this variable initialization ensures that we are not using any variable alredy used by anyone else:
 //this logic checks if each of the variable exists, if not, it defines it as an empty object:
-var com = com || {};
+/*var com = com || {};
 com.zombiecientifico = com.zombiecientifico || {};
 com.zombiecientifico.learning = com.zombiecientifico.learning || {};
-com.zombiecientifico.learning.jsdp = com.zombiecientifico.learning.jsdp || {};
+com.zombiecientifico.learning.jsdp = com.zombiecientifico.learning.jsdp || {};*/
+
+//Module pattern demonstration:
+var chatModule = (function() {
+	//This part is private to this module
+	var leadself = 'Me: ',
+	leadcomputer = "PC: ",
+	aSaid = ["This is a Cyber Chat"],
+	msgYes = "Yes, that's a great idea.",
+	msgNo = "No, that must be a mistake.",
+	aSassyStuff = [
+			"Like mold on books, grow myths on history.",
+			"She moved like a poem and smiled like a sphinx.",
+			"As long as we don’t die, this is gonna be one hell of a story.",
+			"She laughed, and the desert sang.",
+			"You’ve got about as much charm as a dead slug."
+	];
+	//The echo function is in this other part, which makes it part of this Module's function:
+	var echo = function(msg) {
+			aSaid.push("<div>" + msg + "</div>")
+			var aSaidLength = aSaid.length;
+			var start = Math.max(aSaidLength - 6, 0);
+			var out = "";
+			for (var i = start; i < aSaidLength; i++) {
+					out += aSaid[i]
+			}
+			$('#talk span').text(msg);
+			$('.advert').html(out);
+	};
+	//this part is going to be public
+	return {
+		talk: function(msg) {
+        echo(leadself + msg)
+    },
+    replyYesNo: function() {
+        var msg = Math.random() > .5 ? msgYes : msgNo
+        echo(leadcomputer + msg);
+    },
+    saySassyStuff: function() {
+        var msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length)];
+        echo(msg);
+    }
+	};
+})();
+
+$(document).ready(function() {
+	chatModule.talk('Hello computer!');
+	chatModule.replyYesNo();
+	chatModule.saySassyStuff();
+});
 
 //We are going to be creating an object to reduce global scope abuse:
 //This is the One object pattern
 //o = { //This is not a good name for this object, it's too common!
 //Change the name to a namespace, something really large, package like, that ensures that we don't step over anything that already exists. Se above to see pre-requisites of this.
 //This is the namespace pattern:
-com.zombiecientifico.learning.jsdp.pseudochat = {
-    leadself: 'Me: ',
+//com.zombiecientifico.learning.jsdp.pseudochat = {
+    /*leadself: 'Me: ',
     leadcomputer: "PC: ",
     aSaid: ["This is a Cyber Chat"],
     msgYes: "Yes, that's a great idea.",
@@ -22,9 +71,9 @@ com.zombiecientifico.learning.jsdp.pseudochat = {
         "As long as we don’t die, this is gonna be one hell of a story.",
         "She laughed, and the desert sang.",
         "You’ve got about as much charm as a dead slug."
-    ],
+    ],*/
 
-    talk: function(msg) {
+    /*talk: function(msg) {
         this.echo(this.leadself + msg)
     },
     replyYesNo: function() {
@@ -45,8 +94,8 @@ com.zombiecientifico.learning.jsdp.pseudochat = {
         }
         $('#talk span').text(msg);
         $('.advert').html(out);
-    }
-};
+    }*/
+//};
 
 /*
 //Scope was overused
